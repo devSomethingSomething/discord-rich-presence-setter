ea1ccc047fda4ac655fa465119289db006060c5e
devSomethingSomething <61376074+devSomethingSomething@users.noreply.github.com>
2022-01-29 14:02:42 +0200

    Add first version of client

--- src/DiscordRichPresenceSetter/Program.cs
+++ src/DiscordRichPresenceSetter/Program.cs
@@ -1,12 +1,45 @@
-﻿using System;
+﻿using DiscordRPC;
+using System;
 
 namespace DiscordRichPresenceSetter
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        private static DiscordRpcClient discordRpcClient;
+
+        private static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Initialize("applicationID");
+
+            Console.WriteLine("Press any key to close the program");
+
+            Console.ReadKey(true);
+
+            Dispose();
+
+            Console.ReadKey(true);
+        }
+
+        private static void Initialize(string applicationID)
+        {
+            discordRpcClient = new DiscordRpcClient(applicationID);
+
+            discordRpcClient.Initialize();
+
+            SetPresence();
+        }
+
+        private static void SetPresence()
+        {
+            discordRpcClient.SetPresence(new RichPresence()
+            {
+                Details = "Hello World, Discord Edition"
+            });
+        }
+
+        private static void Dispose()
+        {
+            discordRpcClient.Dispose();
         }
     }
 }
